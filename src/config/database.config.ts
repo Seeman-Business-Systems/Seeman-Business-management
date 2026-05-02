2a07450f3162984c611473163096948f35c78ead
PaschalKen <ezenwobikene@gmail.com>
2026-05-02 21:29:09 +0100

    Fix: db config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,20 +5,42 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 export const DatabaseConfig = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (config: ConfigService) => ({
-    type: 'postgres',
-    host: config.get('DB_HOST'),
-    port: config.get<number>('DB_PORT', 5432),
-    username: config.get('DB_USERNAME'),
-    password: config.get('DB_PASSWORD'),
-    database: config.get('DB_NAME'),
-    logging: true,
-    autoLoadEntities: true,
-    namingStrategy: new SnakeNamingStrategy(),
-    synchronize: true,
-    ssl:
-      config.get('DB_SSL', 'false') === 'true'
-        ? { rejectUnauthorized: false }
-        : false,
-  }),
+  // useFactory: (config: ConfigService) => ({
+  //   type: 'postgres',
+  //   host: config.get('DB_HOST'),
+  //   port: config.get<number>('DB_PORT', 5432),
+  //   username: config.get('DB_USERNAME'),
+  //   password: config.get('DB_PASSWORD'),
+  //   database: config.get('DB_NAME'),
+  //   logging: true,
+  //   autoLoadEntities: true,
+  //   namingStrategy: new SnakeNamingStrategy(),
+  //   synchronize: true,
+  //   ssl:
+  //     config.get('DB_SSL', 'false') === 'true'
+  //       ? { rejectUnauthorized: false }
+  //       : false,
+  // }),
+  useFactory: (config: ConfigService) => {
+    const url = config.get<string>('DATABASE_URL');
+    return {
+      type: 'postgres',
+      ...(url
+        ? { url }
+        : {
+            host: config.get<string>('DB_HOST'),
+            port: config.get<number>('DB_PORT', 5432),
+            username: config.get<string>('DB_USERNAME'),
+            password: config.get<string>('DB_PASSWORD'),
+            database: config.get<string>('DB_NAME'),
+          }),
+      autoLoadEntities: true,
+      namingStrategy: new SnakeNamingStrategy(),
+      synchronize: true,
+      ssl:
+        config.get<string>('DB_SSL', 'false') === 'true'
+          ? { rejectUnauthorized: false }
+          : false,
+    };
+  },
 });
